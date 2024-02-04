b5323edd4694386a7b49bea1ec466d5931baf74e
miletic22 <107651545+miletic22@users.noreply.github.com>
2024-02-04 10:08:29 -0800

    Migrate test db files to database.py, create client, session fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,32 +1,8 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 from app import schemas
-from app.config import settings
-from app.main import app
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+from .database import client, session  # noqa: F401
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-client = TestClient(app)
-
-
-def test_create_user():
+def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "hello12345@gmail.com", "password": "password123"}
     )
@@ -34,3 +10,4 @@ def test_create_user():
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello12345@gmail.com"
     assert res.status_code == 201
+    
