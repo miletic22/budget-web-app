5b7633141d8ddbd1cce2f4651fda0e854df4ea8c
miletic22 <107651545+miletic22@users.noreply.github.com>
2024-02-06 15:54:00 -0800

    Add and update CORS

--- app/main.py
+++ app/main.py
@@ -3,11 +3,18 @@
 from fastapi import FastAPI
 
 from .routers import auth, budget, category, transaction, user
-
-# models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(budget.router)
 app.include_router(user.router)
